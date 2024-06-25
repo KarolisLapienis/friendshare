760c94f68d45bb4e39f5dd1324126f07a3a5effc
KaLa <k.lapienis@gmail.com>
2024-06-25 12:38:55 +0300

    naming change

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -9,7 +9,7 @@ export default function TopNav() {
 
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <div>Gallery</div>
+      <div>Friendshare</div>
       <div className="flex flex-row gap-4">
         <SignedOut>
           <SignInButton mode="modal" />
