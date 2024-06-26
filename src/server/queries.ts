68b252cbaa806c2dc4f3b42a08619ebe61196f14
KaLa <k.lapienis@gmail.com>
2024-06-26 18:05:46 +0300

    getImage query added

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -4,11 +4,25 @@ import { auth } from "@clerk/nextjs/server";
 
 export async function getUserImages() {
   const user = auth();
-  if (!user.userId) throw new Error("Unauthorized");
+  if (!user.userId) throw new Error("Unauthenticated user");
 
   const images = await db.query.images.findMany({
     where: (model, { eq }) => eq(model.userId, user.userId),
     orderBy: (model, { desc }) => desc(model.createdAt),
   });
   return images;
+}
+
+export async function getImage(id: number) {
+  const user = auth();
+  if (!user.userId) throw new Error("Unauthenticated user");
+
+  const image = await db.query.images.findFirst({
+    where: (model, { eq }) => eq(model.id, id),
+  });
+
+  if (!image) throw new Error("Image not found");
+  if (image.userId !== user.userId) throw new Error("Unauthorized user");
+
+  return image;
 }
